76d9e1ebfedca2664948aedaea4ea0da7f57c516
chezhian.p <chezhian.p@gmail.com>
2020-05-21 09:51:27 +0530

    android splash screen

--- App.js
+++ App.js
@@ -6,15 +6,19 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Main from './Main';
 import {Provider} from 'react-redux';
 import {configureStore} from './src/AppStore/ConfigureStore';
+import SplashScreen from 'react-native-splash-screen';
 
 const store = configureStore();
 
 const App: () => React$Node = () => {
   console.disableYellowBox = true;
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <Main />
--- android/app/src/main/java/com/xyzpays/MainActivity.java
+++ android/app/src/main/java/com/xyzpays/MainActivity.java
@@ -1,8 +1,17 @@
 package com.xyzpays;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; // Import this.
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
